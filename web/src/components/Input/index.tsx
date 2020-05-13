473972ee08247524b19e7763d0fc06f9f8e69032
guilhermepdc <guilherme.carvalho@edu.pucrs.br>
2020-05-13 15:17:38 -0300

    isolando componentes

---
+++ web/src/components/Input/index.tsx
@@ -0,0 +1,18 @@
+import React, { InputHTMLAttributes } from 'react';
+import { IconBaseProps } from 'react-icons';
+
+import { Container } from './styles';
+
+interface InputPorps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputPorps> = ({ icon: Icon, ...rest }) => (
+  <Container>
+    {Icon && <Icon size={20} />}
+    <input {...rest} />
+  </Container>
+);
+
+export default Input;
