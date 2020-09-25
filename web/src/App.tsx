e1e628e42f8cb537ceee9e5405d54df1736d7f34
guilhermepdc <guilherme.carvalho@edu.pucrs.br>
2020-09-25 12:54:37 -0300

    N3 - Mensagens de toast

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,14 +5,14 @@ import SignIn from './pages/SignIn';
 
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <SignIn />
       {/* <SignUp /> */}
-    </AuthProvider>
+    </AppProvider>
 
     <GlobalStyle />
   </>
