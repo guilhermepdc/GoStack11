cd849dd9cec113c0c06da107b5fc02ef4995c932
guilhermepdc <guilherme.carvalho@edu.pucrs.br>
2020-10-06 12:25:55 -0300

    N3 - Autenticacao e cadastro / Input & Button

---
+++ mobile/src/components/Input/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { TextInputProps } from 'react-native';
+
+import { Container, TextInput, Icon } from './styles';
+
+interface InputPorps extends TextInputProps {
+  name: string;
+  icon: string;
+}
+
+const Input: React.FC<InputPorps> = ({ name, icon, ...rest }) => (
+  <Container>
+    <Icon name={icon} size={20} color="#666360" />
+
+    <TextInput
+      keyboardAppearance="dark"
+      placeholderTextColor="#666360"
+      {...rest}
+    />
+  </Container>
+);
+
+export default Input;
